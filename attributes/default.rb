955d85df37a4b2a476386fef9ec5b341a7ed00cb
kirkl <kirkl@slalom.com>
2015-05-06 14:39:31 -0700

    Add git and all its dependencies and a few others like ant

---
+++ attributes/default.rb
@@ -0,0 +1,27 @@
+default[:jenkins][:server][:plugins] = %w(
+  ant
+  nant
+  mailer
+  javadoc
+  token-macro
+  maven-plugin
+  run-condition
+  conditional-buildstep
+  matrix-project
+  rebuild
+  promoted-builds
+  mapdb-api
+  credentials
+  ssh-credentials
+  scm-api
+  subversion
+  parameterized-trigger
+  git-client
+  git
+  maven-plugin
+  maven-info
+  dashboard-view
+  jquery
+  build-pipeline-plugin
+)
+
